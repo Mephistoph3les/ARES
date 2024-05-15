4618cd219eb115042024b5f96f4eaf62cfd4b741
Kevin <kevindojan@gmail.com>
2024-05-15 16:27:10 +0200

    implemented gpu computing via jit

--- Metal Sheets/scattering_calibration_plot_metal_sheets_nickel.py
+++ Metal Sheets/scattering_calibration_plot_metal_sheets_nickel.py
@@ -1,23 +1,21 @@
 # execute with      python3 scattering_calibration_plot_metal_sheets.py
 
+from numba import jit, cuda 
 import pandas as pd 
-import sys
 import matplotlib.pyplot as plt 
 import math
 import numpy as np
 import time
-from multiprocessing import Pool, cpu_count
 
 
-def time_cut_of_data(file_name, logbook_name, log_starting_point, file_number):
-    # sourcery skip: for-index-replacement, list-comprehension
-    data = pd.read_csv(file_name, sep='\s+', names= ['Event', 'Macropulse', 'Width', 'Timestamp'])
+
+def time_cut_of_data(file_name, logbook_name, log_starting_point, file_number, list_of_x_positions, list_of_material_thicknesses):
+    data = pd.read_csv(file_name, sep='\s+', names= ['Event', 'Timestamp', 'Width', 'Intensity'])
     logbook = pd.read_csv(logbook_name, sep='\s+', skiprows=(16+log_starting_point-1) , names= ['Event', 'X Position (mm)', 'Y Index', '[ADD 1] Position (deg)', '[ADD 2] Position (mm)' , '[ADD 3] Count ()', '[ADD 4] Charge (pC)', 'UNIX Time Stamp (seconds)'])
     logbook['Event'] = logbook['Event'].astype(int)
     logbook_list = ([])
     help_list = ([])
 
-    list_of_x_positions = ([35, 51.5 , 68, 84.5, 101, 117.5, 134, 150.5, 167, 183.5, 200, 216.5, 233, 249.5])       #266 is skipped because that is the second zero/reference measurement
     for i in range(len(list_of_x_positions)):
         if i==0:
             help_list.extend((logbook.loc[logbook['X Position (mm)'] == list_of_x_positions[i]], logbook.loc[logbook['X Position (mm)'] == 266]))
@@ -38,46 +36,53 @@ def time_cut_of_data(file_name, logbook_name, log_starting_point, file_number):
     elif file_number==2:        #file 2 is aluminum shortscan2
         timestamp_offset = 1712235885.5
         output_filename = 'Aluminum_data/output_measurement_221_amended.csv'
-        
+
     data_timestamps = data.loc[:, "Timestamp"]
     length_of_dataset = data_timestamps.shape[0]
-    data_timestamp_array = data_timestamps.to_numpy()  
-    thickness_region_allocation = ([])
+    data_timestamp_array = data_timestamps.to_numpy()
     
-    for i in range(length_of_dataset-1):                                  #iteration over every datapoint of the measurement
-        if i==100:
-            break
+    print(list_of_x_positions)
+    
+    scan = timestamp_scan(list_of_x_positions, list_of_material_thicknesses, list_of_boundaries, timestamp_offset, length_of_dataset, data_timestamp_array)
+    thickness_region_allocation = scan[0]
+    thickness_allocation = scan[1]
+    rows_to_drop = data.shape[0]- len(thickness_region_allocation)
+    data.drop(data.tail(rows_to_drop).index, inplace = True)
+    print("dropped ", rows_to_drop, " rows from data")
+    data.insert(4, "Stage x Position", thickness_region_allocation, True)
+    data.insert(5, "Material Thickness", thickness_allocation, True)
+    print(data)
+    data.to_csv(output_filename, sep=',', index=False) 
+
+    return data
+@jit
+def timestamp_scan(list_of_x_positions, list_of_material_thicknesses, list_of_boundaries, timestamp_offset, length_of_dataset, data_timestamp_array):
+    thickness_region_allocation = ([])
+    thickness_allocation = ([])
+    
+    for i in range(length_of_dataset):                                  #iteration over every datapoint of the measurement
         found = False
-        for m in range(len(list_of_boundaries)-1):                        #iteration over every x position of the stage in logfile corresponding to material thickness 
+        for m in range(len(list_of_boundaries)):                        #iteration over every x position of the stage in logfile corresponding to material thickness 
             if found == True:
                 break
-            for l in range(len(list_of_boundaries[m][0])-1):              #iteration over every start and end point of measurement repetition
+            for l in range(len(list_of_boundaries[m][0])):              #iteration over every start and end point of measurement repetition
                 if found == True:
                     break
                 start = list_of_boundaries[m][0][l] - timestamp_offset
                 end = list_of_boundaries[m][1][l] - timestamp_offset
                 print("Comparing:", start, "<= ", data_timestamp_array[i], "<=", end)
                 if start <= data_timestamp_array[i] <= end:
-                    thickness_region_allocation.append(list_of_x_positions[m])
+                    thickness_region_allocation.append(str(list_of_x_positions[m]))
+                    thickness_allocation.append(str(list_of_material_thicknesses[m]))
                     print("added", list_of_x_positions[m], "to slot: ", i)
                     found = True
                 else:
                     print("skipped width for: ", i)
         if found == False:
-            thickness_region_allocation.append("NaN")
-            print("added", list_of_x_positions[m], "to slot: ", i)
-    
-    print("Final thickness region allocation:" , thickness_region_allocation)
-    print("Then length of that list is: ", len(thickness_region_allocation))
-    
-    rows_to_drop = data.shape[0]- len(thickness_region_allocation)
-    data.drop(data.tail(rows_to_drop).index, inplace = True)
-    print("dropped ", rows_to_drop, " rows from data")
-    data.insert(4, "Material Thickness", thickness_region_allocation, True)
-    print(data)
-    data.to_csv(output_filename, index=False) 
-    
-    return(data)
+            thickness_region_allocation.append('NaN')
+            thickness_allocation.append('NaN')
+
+    return(thickness_region_allocation, thickness_allocation)
 
 def get_boundaries(logbook_list):    
     log = logbook_list.loc[:, "Event"]              #Take a slice with only the Event numbers
@@ -110,9 +115,9 @@ def get_boundaries(logbook_list):
 def get_mean(file_name, reference_file_name):
     
     #reading in the data
-    data = pd.read_csv(file_name, sep='\s+', names= ['Event', 'Macropulse', 'Width', 'Timestamp'])
+    data = pd.read_csv(file_name, sep='\s+', names= ['Event', 'Timestamp', 'Width', 'Intensity'])
     #reading in reference beam data to subtract from actual data for beam widening
-    reference = pd.read_csv(reference_file_name, sep='\s+', names= ['Event', 'Macropulse', 'Width', 'Timestamp'])
+    reference = pd.read_csv(reference_file_name, sep='\s+', names= ['Event', 'Timestamp', 'Width', 'Intensity'])
 
     ref_mean = reference.loc[:, 'Width'].mean()
     
@@ -124,6 +129,35 @@ def get_mean(file_name, reference_file_name):
     N = data.shape[0]
 
     return data_mean**2, data_std, N
+
+def separate_data(data, list_of_x_positions):
+    data_separated_list = ([])
+    for i in range(len(list_of_x_positions)):
+        data_separated_list.append(data.loc[data['Material Thickness'] == list_of_x_positions[i]])
+    return data_separated_list
+
+def ladderplot(data_list, dataset, list_of_x_positions):
+    material = "Nickel" if dataset==0 else "Aluminum"
+    widths=[]
+    material_thicknesses=[]
+    help=[]
+    for i in range(len(list_of_x_positions)-1):
+        help = data_list[i].loc[:, "Width"]
+        help = help.to_numpy()
+        widths.append(help)
+        
+        help2 = data_list[i].loc[:, "Material Thickness"]
+        help2 = help2.to_numpy()
+        material_thicknesses.append(help2)
+
+    fig, ax = plt.subplots(figsize=(10,10), layout='constrained')
+    for i in range(len(list_of_x_positions)-1):
+        ax.scatter(widths[i], material_thicknesses[i], label = ('Stage x position: ', list_of_x_positions[i]))
+    ax.set_xlabel('Width', style='normal')
+    ax.set_ylabel('Material Thickness')
+    ax.set_title(('Ladderplot', material, ' of dataset ', dataset ))
+    ax.legend()
+    plt.show()
 
 def highland(material_budget):
     
@@ -138,6 +172,11 @@ def highland(material_budget):
 
 if __name__ == '__main__':
     start_time = time.time()
+    list_of_x_positions = ([35.0, 51.5 , 68.0, 84.5, 101.0, 117.5, 134.0, 150.5, 167.0, 183.5, 200.0, 216.5, 233.0, 249.5])       #266 is skipped because that is the second zero/reference measurement
+    radiation_length_nickel = 35.3                                      #in mm
+    error_in_radiation_length = 0.05                                    # ~2% error
+    radiation_length_aluminum = 8.9                                     #in mm
+    list_of_material_thicknesses = ([0.0 ,0.025, 0.075, 0.05, 0.15, 0.1, 0.35, 0.25, 0.75, 0.5, 1.5, 1.0, 3.0, 2.0])     #in mm (CAREFUL! ALUMINUM HAS DIFFERENT THICKNESSES WHICH ARE NOT IMPLEMENTED RN)
     files_list=(['Nickel_data/output_measurement_203.dat', 'Aluminum_data/output_measurement_211.dat', 'Aluminum_data/output_measurement_221.dat'])
     ask = input("Do you want to do the time slicing of measurement data (no skips to analysis)?  ")
     if ask.lower() in ["y","yes"]:
@@ -147,30 +186,32 @@ if __name__ == '__main__':
         if answer.lower() in ["y","yes"]:
             logbook_files_list = (['Nickel_data/eCTLogger_ni_overnight.txt', 'Aluminum_data/eCTLogger_al_shortscan1.txt', 'Aluminum_data/eCTLogger_al_shortscan2.txt'])
             list_of_log_starting_points = ([17, 654, 492])
-            data = time_cut_of_data(files_list[dataset],logbook_files_list[dataset],list_of_log_starting_points[dataset], dataset)
+            data = time_cut_of_data(files_list[dataset],logbook_files_list[dataset],list_of_log_starting_points[dataset], dataset, list_of_x_positions, list_of_material_thicknesses)
     else:
         print("0 for nickel overnight \n1 for aluminum scan1 \n2 for aluminum scan2 ")
-        ask = input("Which dataset do you want to analyse? ")
-        data = pd.read_csv(files_list[ask], sep='\s+', names= ['Event', 'Macropulse', 'Width', 'Timestamp', 'Material Thickness'])
+        dataset = int(input("Which dataset do you want to analyse? "))
+        files_list_amen=(['Nickel_data/output_measurement_203_amended.csv', 'Aluminum_data/output_measurement_211_amended.csv', 'Aluminum_data/output_measurement_221_amended.csv'])
+        data = pd.read_csv(files_list_amen[dataset], sep=',', skiprows=(1), names= ['Event', 'Timestamp', 'Width', 'Intensity', 'Stage x Position', 'Material Thickness'])
+        data_list = separate_data(data, list_of_x_positions)        
+        ask = input("Do you want to create a ladderplot? ")
+        if ask.lower() in ["y","yes"]:
+            ladderplot(data_list, dataset, list_of_x_positions)
+
 
     
-    radiation_length_nickel = 35.3                                      #in mm
-    error_in_radiation_length = 0.05                                    # ~2% error
-    radiation_length_aluminum = 8.9                                     #in mm
-    list_of_material_thicknesses_overnight = ([0.025, 0.075, 0.05, 0.15, 0.1, 0.35, 0.25, 0.75, 0.5, 1.5, 1, 3, 2, 3, 1, 1.5, 0.5, 0.75, 0.25, 0.35, 0.1, 0.15, 0.05, 0.075, 0.025])     #in mm
 #    list_of_material_thicknesses_shortscan = ([2, 3, 1, 1.5, 0.5, 0.75, 0.25, 0.35, 0.1, 0.15, 0.05, 0.075, 0.025, 0])     #in mm
     error_in_material_thickness = 0.0005                                   #error in mm
     
-    m = len(list_of_material_thicknesses_overnight)
+    m = len(list_of_material_thicknesses)
 
     material_budget_list = ([])
     highland_prediction_list = ([])
     material_budget_error_list = ([])
-    for i in range (m):
-        material_budget_list.append(list_of_material_thicknesses_overnight[i]/radiation_length_nickel)
-        material_budget_error = np.sqrt(((error_in_material_thickness/radiation_length_nickel)**2+(error_in_radiation_length*list_of_material_thicknesses_overnight[i]/(radiation_length_nickel)**2)**2))
+    for i in range (1, m-1):
+        material_budget_list.append(list_of_material_thicknesses[i]/radiation_length_nickel)
+        material_budget_error = np.sqrt(((error_in_material_thickness/radiation_length_nickel)**2+(error_in_radiation_length*list_of_material_thicknesses[i]/(radiation_length_nickel)**2)**2))
         material_budget_error_list.append(material_budget_error)
-        highland_prediction_list.append(highland(material_budget_list[i])[0])
+        highland_prediction_list.append(highland(material_budget_list[i-1])[0])
     #print("Thats the list of material budgets: ")
     #print(material_budget_list)
     #print("Thats the list of material budget errors: ")
@@ -195,4 +236,5 @@ if __name__ == '__main__':
     #ax.legend()
     #plt.show()
     end_time = time.time()
-    print("Computing time was: ", end_time - start_time, " s")
+    print("Computing time was:  ", round(end_time - start_time, 2), " s")
+    print("Which in minutes is: ", round((end_time - start_time)/60, 2) , " min")
